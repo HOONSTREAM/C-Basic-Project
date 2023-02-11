52fc863a24b373908495cc80da5e249db490eda0
HOONSTREAM <123620703+HOONSTREAM@users.noreply.github.com>
2023-02-11 19:40:36 +0900

    멤버함수포인터(객체호출,주소호출)

---
+++ 멤버함수 포인터.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+
+private :
+
+	int x, y;
+
+public :
+
+	explicit Point(int _x = 0, int _y = 0) :x(_x),y(_y) {}
+	void Print() const { cout << x << "," << y << endl; }
+	void PrintInt(int n) { cout << "테스트 정수 : " << n << endl; }
+};
+
+int main(void) {
+
+	Point pt(2, 3);
+	Point* p = &pt;
+
+	void (Point::* pf1)() const;
+	pf1 = &Point::Print;
+
+	void (Point:: * pf2)(int);
+	pf2 = &Point::PrintInt;
+
+	pt.Print();
+	pt.PrintInt(10);
+	cout << endl;
+
+	(pt.*pf1)();
+	(pt.*pf2)(10);
+
+	(p->*pf1)();
+	(p->*pf2)(10);
+
+	return 0;
+
+
+
+}
