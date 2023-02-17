2d68e784e29004dfa0e7223ba714c94580522d22
HOONSTREAM <123620703+HOONSTREAM@users.noreply.github.com>
2023-02-17 17:14:00 +0900

    for_each() 명시적호출

---
+++ for_each() 명시호출.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template<class IterT, class Func>
+void For_each(IterT begin, IterT end, Func pf) {
+
+	while (begin != end) {
+
+		pf(*begin++);
+	}
+}
+
+template<class T>
+void Print(T data) {
+
+	cout << data << " ";
+
+}
+
+int main(void) {
+
+	int arr[5] = { 10,20,30,40,50 };
+	For_each(arr, arr + 5, Print<int>);
+
+	cout << endl;
+
+	string sarr[3] = { "abc","Hello","ABCDE" };
+	For_each(sarr, sarr + 3, Print<string>);
+
+
+	return 0;
+}
